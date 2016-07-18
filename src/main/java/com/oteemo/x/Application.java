f5b28bd57ca29b6150458205d29a7238cf74eb29
Administrator <admin@local.host>
2016-07-17 20:32:01 -0400

    bootable jar

--- src/main/java/com/oteemo/x/Application.java
+++ src/main/java/com/oteemo/x/Application.java
@@ -1,3 +1,24 @@
 package com.oteemo.x;
 
-public interface Application {}
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public interface Application {
+	
+	@SpringBootApplication
+	public class ApplicationRunner extends SpringBootServletInitializer {
+	 
+	    public static void main(String[] args) {
+	        SpringApplication.run(ApplicationRunner.class, args);
+	    }
+	 
+	    @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(ApplicationRunner.class);
+	    }
+	 
+	}
+	
+}
