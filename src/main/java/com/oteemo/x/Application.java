1d3abd7303a74ba4c12f55afe1d805328bfc1ca4
Administrator <admin@local.host>
2016-07-19 10:32:40 -0400

    revert to war

--- src/main/java/com/oteemo/x/Application.java
+++ src/main/java/com/oteemo/x/Application.java
@@ -1,24 +1,3 @@
 package com.oteemo.x;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-
-public interface Application {
-	
-	@SpringBootApplication
-	public class ApplicationRunner extends SpringBootServletInitializer {
-	 
-	    public static void main(String[] args) {
-	        SpringApplication.run(ApplicationRunner.class, args);
-	    }
-	 
-	    @Override
-	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-	        return builder.sources(ApplicationRunner.class);
-	    }
-	 
-	}
-	
-}
+public interface Application {}
